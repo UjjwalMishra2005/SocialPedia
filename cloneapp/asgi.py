136e48f54f2f3aec58bf5924bec64e36a031ed07
Ujjwalmishra2005 <ujjwalmishra9936@gmail.com>
2025-04-06 10:10:11 +0530

    changes to asgi.py

--- cloneapp/asgi.py
+++ cloneapp/asgi.py
@@ -10,10 +10,20 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter,URLRouter
 from channels.auth import AuthMiddlewareStack
 import base.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cloneapp.settings')
+# ProtocolTypeRouter takes a mapping of protocol type names to other Application instances, and dispatches to the right one based on protocol name (or raises an error)
+application = ProtocolTypeRouter({
+    'http':get_asgi_application(),
+    'websocket':AuthMiddlewareStack(
+        URLRouter(
+            base.routing.websocket_urlpatterns
+        )
+    )
 
-application = get_asgi_application()
+})
+
+
