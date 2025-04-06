ea44f584fc85932b4ca0d6c8d5a8cf1e2c1c1d28
Ujjwalmishra2005 <ujjwalmishra9936@gmail.com>
2025-04-06 10:48:07 +0530

    Changes done in routing file and created base consumer -handler

---
+++ base/consumers.py
@@ -0,0 +1,16 @@
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+class BaseConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+
+        await self.channel_layer.group_add(
+            "New message",
+            self.channel_name
+        )
+        await self.accept()
+        await self.send(text_data = json.dumps({
+            'type':'connection_established',
+            'message':'Connected'
+            
+        }))
