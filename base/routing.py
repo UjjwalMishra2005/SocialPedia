94a12f16266c4a96887a520d588f0e3d50c545f7
Ujjwalmishra2005 <ujjwalmishra9936@gmail.com>
2025-06-01 18:26:47 +0530

    changes

--- base/routing.py
+++ base/routing.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from base.consumers import *
+from base.consumers import BaseConsumer
 websocket_urlpatterns=[
     path('ws/socket-server/<chatroom_name>', BaseConsumer.as_asgi())
 ]
